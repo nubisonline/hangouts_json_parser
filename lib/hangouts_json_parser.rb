87332e38f7a639857ed33c6627270741703f2c1f
Tim van Dalen <Tim@timvdalen.nl>
2016-02-23 20:16:59 +0100

    Parse converstations from converstation_state

--- lib/hangouts_json_parser.rb
+++ lib/hangouts_json_parser.rb
@@ -4,4 +4,13 @@ require "hangouts_json_parser/conversation"
 
 # Parse JSON formatted Hangouts data into more useful data structures
 module HangoutsJsonParser
+	# Parses conversation state into an array of Conversations
+	#
+	# @param state [Array<Hash>] converstation_state from the Takeout data
+	# @return [Array<Conversation>] all conversations in the state
+	def self.parse_conversation_state state
+		state.map do |s|
+			Conversation.from_state s['conversation_state']
+		end
+	end
 end
--- lib/hangouts_json_parser/conversation.rb
+++ lib/hangouts_json_parser/conversation.rb
@@ -7,5 +7,30 @@ module HangoutsJsonParser
 		attr_reader :type
 		# @return [Array<User>] the Users that are participating in this chat
 		attr_reader :participants
+
+		def initialize id, type, participants
+			@id = id
+			@type = type
+			@participants = participants
+		end
+
+		# Creates a Conversation based on a conversation_state object
+		# @param state [Hash] conversation_state
+		# @return [Conversation] resulting Conversation
+		def self.from_state state
+			Conversation.new state['conversation']['id']['id'], type_from_string(state['conversation']['type']), []
+		end
+
+		private
+		def self.type_from_string str
+			case str
+			when "STICKY_ONE_TO_ONE"
+				:private
+			when "GROUP"
+				:group
+			else
+				:unknown
+			end
+		end
 	end
 end
